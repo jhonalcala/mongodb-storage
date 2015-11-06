37ace425f2b8e3a7b18b4ceb48b7c362128570bd
Benj Sicam <bsicam@reekoh.com>
2015-11-06 11:13:46 +0800

    Close event handler enhancements
    Now using Node.js domains on close event for better error handling and
    prevent app crashes.

--- app.js
+++ app.js
@@ -36,8 +36,19 @@ platform.on('data', function (data) {
  * Event to listen to in order to gracefully release all resources bound to this service.
  */
 platform.on('close', function () {
-	db.close(true, function () {
+	var domain = require('domain');
+	var d = domain.create();
+
+	d.on('error', function(error) {
+		console.error(error);
+		platform.handleException(error);
 		platform.notifyClose();
+	});
+
+	d.run(function() {
+		db.close(true, function () {
+			platform.notifyClose();
+		});
 	});
 });
 
